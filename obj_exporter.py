948b820d08876901ab41e0713f407f9ab3b30a06
yami-five <konrad.rykala.94@gmail.com>
2024-08-12 21:32:16 +0200

    fixed torus

--- obj_exporter.py
+++ obj_exporter.py
@@ -2,29 +2,30 @@ vertices=[]
 faces=[]
 vt=[]
 uv=[]
-with open("assets/torus3_8.obj") as f:
-    while line:=f.readline():
-        if line[0:2]=='v ':
-            vertex=line.split()
-            vertices.append(round(float(vertex[1]),2))
-            vertices.append(round(float(vertex[2]),2))
-            vertices.append(round(float(vertex[3]),2))
-        elif line[0]=='f':
-            face=line.split()
-            faces.append(int(face[3].split('/')[0])-1)
-            uv.append(int(face[3].split('/')[1])-1)
-            faces.append(int(face[2].split('/')[0])-1)
-            uv.append(int(face[2].split('/')[1])-1)
-            faces.append(int(face[1].split('/')[0])-1)
-            uv.append(int(face[1].split('/')[1])-1)
-        elif line[0:2]=='vt':
-            vt.append(round(float(line.split()[1]),2))
-            vt.append(round(float(line.split()[2]),2))
-text=""
-for x in vertices:
-    text+=str(x)+','
+for x in range(1,9):
+    with open("assets/torus3_"+str(x)+".obj") as f:
+        while line:=f.readline():
+            if line[0:2]=='v ':
+                vertex=line.split()
+                vertices.append(round(float(vertex[1]),2))
+                vertices.append(round(float(vertex[2]),2))
+                vertices.append(round(float(vertex[3]),2))
+            elif line[0]=='f':
+                face=line.split()
+                faces.append(int(face[3].split('/')[0])-1)
+                uv.append(int(face[3].split('/')[1])-1)
+                faces.append(int(face[2].split('/')[0])-1)
+                uv.append(int(face[2].split('/')[1])-1)
+                faces.append(int(face[1].split('/')[0])-1)
+                uv.append(int(face[1].split('/')[1])-1)
+            elif line[0:2]=='vt':
+                vt.append(round(float(line.split()[1]),2))
+                vt.append(round(float(line.split()[2]),2))
+    text=""
+    for x in vertices:
+        text+=str(x)+','
 print(text[:-1])
-print(len(vertices)//3)
+# print(len(vertices)//3)
 # text=""
 # for x in faces:
 #     text+=str(x)+','
